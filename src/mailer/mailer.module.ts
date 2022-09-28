3eb81647a6fcca24f47ba5cc5401c5c687b6a683
michalbusso <micheth.y@gmail.com>
2022-09-28 22:52:47 +0300

    updates

--- src/mailer/mailer.module.ts
+++ src/mailer/mailer.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
+import { MailerModule as MailModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 
 @Module({
-  controllers: [],
-  providers: []
-})
+    imports: [
+      MailModule.forRootAsync({
+        useFactory: () => ({
+          transport: 'smtps://user@domain.com:pass@smtp.domain.com',
+          defaults: {
+            from: '"nest-modules" <modules@nestjs.com>',
+          },
+          template: {
+            dir: __dirname + '/templates',
+            adapter: new HandlebarsAdapter(),
+            options: {
+              strict: true,
+            },
+          },
+        }),
+      }),
+    ],
+  })
 export class MailerModule {}
--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from "@nestjs/common";
-
-
+import { Injectable } from '@nestjs/common';
+import { MailerService as MailService } from '@nestjs-modules/mailer';
 
 @Injectable()
+export class MailerService {
+  constructor(private readonly mailerService: MailService) {}
 
-export class MailerService{
-    
+  public example(): void {
+    this.mailerService
+      .sendMail({
+        to: 'test@nestjs.com',
+        from: 'noreply@nestjs.com',
+        subject: 'your order completed',
+        template: 'welcome', // The `.pug`, `.ejs` or `.hbs` extension is appended automatically.
+        context: {
+          // Data to be sent to template engine.
+          code: 'cf1a3f828287',
+          username: 'john doe',
+        },
+      })
+      .then(() => {})
+      .catch(() => {});
+  }
 }
